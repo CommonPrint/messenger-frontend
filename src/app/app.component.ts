b1c49729c3b7ce31c3befbfbb9b8af8e30eae5dc
Adlet <pioneermove@gmail.com>
2024-10-30 02:10:19 +0600

    feat: created frontend pages for our Messenger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import {
   NgbAccordionBody,
   NgbAccordionButton,
@@ -10,29 +9,49 @@ import {
   NgbAccordionItem,
   NgbToast
 } from "@ng-bootstrap/ng-bootstrap";
-
+import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
-
+import {Oauth2AuthService} from "./auth/oauth2-auth.service";
+import {NavbarComponent} from "./layout/navbar/navbar.component";
+import {ToastService} from "./shared/toast/toast.service";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import {ConversationsComponent} from "./conversations/conversations.component";
+import {HeaderComponent} from "./layout/header/header.component";
+import {SendComponent} from "./messages/send/send.component";
+import {MessagesComponent} from "./messages/messages.component";
 
 @Component({
-  selector: 'app-root',
+  selector: 'wac-root',
   standalone: true,
-  imports: [RouterOutlet, NgbAccordionDirective, NgbAccordionItem, NgbAccordionHeader, NgbAccordionButton, NgbAccordionCollapse, NgbAccordionBody],
+  imports: [RouterOutlet, NgbAccordionDirective, NgbAccordionItem, NgbAccordionHeader, NgbAccordionButton, NgbAccordionCollapse, NgbAccordionBody, FaIconComponent, NavbarComponent, NgbToast, ConversationsComponent, HeaderComponent, SendComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  title = 'messenger-frontend';
+  title = 'messenger-front';
 
   private faIconLibrary = inject(FaIconLibrary);
+  private oauth2Service = inject(Oauth2AuthService);
+  toastService = inject(ToastService);
 
   ngOnInit(): void {
     this.initFontAwesome();
+    this.initAuthentication();
+    this.configDayJs();
   }
 
+  private configDayJs() {
+    dayjs.extend(relativeTime);
+  }
+
+  private initAuthentication(): void {
+    this.oauth2Service.initAuthentication();
+  }
 
   private initFontAwesome() {
     this.faIconLibrary.addIcons(...fontAwesomeIcons);
   }
 
+  protected readonly removeEventListener = removeEventListener;
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  keycloak: {
+    url: "http://localhost:9080",
+    realm: "messenger",
+    clientId: "messenger"
+  },
+  API_URL: "http://localhost:4200/api",
+};
